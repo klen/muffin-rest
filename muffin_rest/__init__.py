cb2a16561081224f8f2078b85678315142fdb902
Kirill Klenov <horneds@gmail.com>
2015-07-01 18:07:12 +0300

    Bump version: 0.0.23 → 0.0.24

--- muffin_rest/__init__.py
+++ muffin_rest/__init__.py
@@ -5,7 +5,7 @@ from muffin import HTTPNotFound, HTTPBadRequest, HTTPForbidden
 # Package information
 # ===================
 
-__version__ = "0.0.23"
+__version__ = "0.0.24"
 __project__ = "muffin-rest"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
