99ceb9023ecd71902b8a27f462004301b937a6ab
Kirill Klenov <horneds@gmail.com>
2015-07-27 18:59:23 +0300

    Bump version: 0.0.30 → 0.0.31

--- muffin_rest/__init__.py
+++ muffin_rest/__init__.py
@@ -6,7 +6,7 @@ import ujson
 # Package information
 # ===================
 
-__version__ = "0.0.30"
+__version__ = "0.0.31"
 __project__ = "muffin-rest"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
