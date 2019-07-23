9cbf12a3bfae9f34b26db4028212ca8ecff54dc3
Kirill Klenov <horneds@gmail.com>
2019-07-23 11:38:33 +0300

    Bump version: 1.6.2 → 1.6.3

--- muffin_rest/__init__.py
+++ muffin_rest/__init__.py
@@ -2,7 +2,7 @@
 # Package information
 # ===================
 
-__version__ = "1.6.2"
+__version__ = "1.6.3"
 __project__ = "muffin-rest"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
