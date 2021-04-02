06ff9ed2db82006486d1c42cab7b051a94c42f1a
Kirill Klenov <horneds@gmail.com>
2021-04-02 12:50:56 +0300

    Bump version: 2.5.4 → 2.6.0

--- muffin_rest/__init__.py
+++ muffin_rest/__init__.py
@@ -1,6 +1,6 @@
 """REST helpers for Muffin Framework."""
 
-__version__ = "2.5.4"
+__version__ = "2.6.0"
 __project__ = "muffin-rest"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
